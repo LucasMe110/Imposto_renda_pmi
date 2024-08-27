6e9061c56ba2ed0dc4502b8bed375347c8a24ec7
Lucas Mello <lucasmellomo@gmail.com>
2024-08-27 20:30:57 -0300

    Mensagem do commit

--- src/send_verification_email.py
+++ src/send_verification_email.py
@@ -3,14 +3,18 @@ from email.message import EmailMessage
 
 def send_verification_email(email, codigo_aleatorio):
     msg = EmailMessage()
-    msg['Subject'] = 'Código de Verificação'
+    msg['Subject'] = 'Código de Verificação - Contabilize Bem'
     msg['From'] = 'lucasmellomo@gmail.com'
     msg['To'] = email
 
     html_content = f"""
     <html>
         <body>
-            <p>Oi! Seu código de verificação é: <b style="font-size: 24px;">{codigo_aleatorio}</b></p>
+            <p>Oi! Você recebeu este e-mail da <b>Contabilize Bem</b>.</p>
+            <p>Seu código de verificação é: <b style="font-size: 24px;">{codigo_aleatorio}</b></p>
+            <p>Se você não reconhece este e-mail, por favor, ignore-o.</p>
+            <p>Obrigado,</p>
+            <p><b>Contabilize Bem</b></p>
         </body>
     </html>
     """
